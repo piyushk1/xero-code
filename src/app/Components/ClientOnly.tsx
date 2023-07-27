2588440ed6b53192382730b790d37f39a243c6d6
Piyush <piyushkukade1@gmail.com>
2023-07-27 23:32:04 +0530

    Landing Page Updated

---
+++ src/app/Components/ClientOnly.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+
+interface ClientOnlyProps {
+  children: React.ReactNode;
+}
+
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  return isMounted ? <>{children}</> : null;
+};
+
+export default ClientOnly;
