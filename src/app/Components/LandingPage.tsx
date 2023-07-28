864ed8a948bebc50873f2c2f63029affe161f754
Piyush <piyushkukade1@gmail.com>
2023-07-28 22:58:32 +0530

    LandingPage Dashboard completed

--- src/app/Components/LandingPage.tsx
+++ src/app/Components/LandingPage.tsx
@@ -1,23 +1,26 @@
 
 "use client"
-import { useEffect, useState,ChangeEvent} from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import '../Components/LandingPage.css';
 import ClientOnly from './ClientOnly';
-
 import middleImage from '../../../public/assets/middleImage.png';
 import stackPurple from '../../../public/assets/StackPurple.png';
 import stackPink from '../../../public/assets/StackPink.png';
 import ellipse2 from '../../../public/assets/Ellipse 2.png';
-import '../Components/LandingPage.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-const arcVariants = {
-  initial: { pathLength: 0 },
-  animate: {
-    pathLength: 1,
-    transition: { duration: 1, ease: 'linear' },
-  },
-};
+
+//for drawing arcs
+// const arcVariants = {
+//   initial: { pathLength: 0 },
+//   animate: {
+//     pathLength: 1,
+//     transition: { duration: 1, ease: 'linear' },
+//   },
+// };
 
 export default function LandingPage() {
   const [showHeaderText, setShowHeaderText] = useState(false);
@@ -35,136 +38,231 @@ export default function LandingPage() {
   const [showAccessText, setShowAccessText] = useState(false);
   const [showFooter, setShowFooter] = useState(false);
   const [scrollAnimation, setScrollAnimation] = useState(false);
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name1, setName1] = useState('');
+  const [email1, setEmail1] = useState('');
+  const [name2, setName2] = useState('');
+  const [email2, setEmail2] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [nameError, setNameError] = useState('');
   const [emailError, setEmailError] = useState('');
+  const [submitSuccess, setSubmitSuccess] = useState(false);
+  const [submitError, setSubmitError] = useState('');
 
-
-  // Function to handle changes in the name input field
-  const handleNameChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
+  
+  const handleNameChange1 = (e:any) => {
+    setName1(e.target.value);
   };
 
-  // Function to handle changes in the email input field
-  const handleEmailChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.target.value);
+  const handleEmailChange1 = (e:any) => {
+    setEmail1(e.target.value);
+  };
+  const handleNameChange2 = (e:any) => {
+    setName2(e.target.value);
   };
 
- // Function to handle form submission
- const handleSubmit = (e: any) => {
-  e.preventDefault();
-
-  setNameError('');
-  setEmailError('');
-
-  let valid = true;
-
-  if (name.trim() === '') {
-    setNameError('Please enter your name.');
-    valid = false;
-  }
-
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    setEmailError('Please enter a valid email address.');
-    valid = false;
-  }
-
-  if (!valid) {
-    return;
-  }
+  const handleEmailChange2 = (e:any) => {
+    setEmail2(e.target.value);
+  };
 
 
-  console.log('Name:', name);
-  console.log('Email:', email);
-  setName('');
-  setEmail('');
-};
+  // Function to handle second form submission
+  const handleSubmit1 = async (e:any) => {
+    e.preventDefault();
+
+    setNameError('');
+    setEmailError('');
+
+    let valid = true;
+
+    if (name1.trim() === '') {
+      setNameError('Please enter your name.');
+      valid = false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email1)) {
+      setEmailError('Please enter a valid email address.');
+      valid = false;
+    }
+
+    if (!valid) {
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch('http://localhost:5000/api/submitForm', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name1, email1 }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSubmitSuccess(true);
+        setSubmitError('');
+        toast.success('Form submitted successfully!');
+
+      } else {
+        setSubmitError(data.error || 'Error submitting the form.');
+        setSubmitSuccess(false);
+        toast.error(data.error || 'Error submitting the form.');
+
+      }
+    } catch (err) {
+      setSubmitError('Error submitting the form.');
+      setSubmitSuccess(false);
+      toast.error('Error submitting the form.');
+
+    }
+
+    setIsSubmitting(false);
+  };
+
+   // Function to handle second form submission
+   const handleSubmit2 = async (e:any) => {
+    e.preventDefault();
+
+    setNameError('');
+    setEmailError('');
+
+    let valid = true;
+
+    if (name2.trim() === '') {
+      setNameError('Please enter your name.');
+      valid = false;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email2)) {
+      setEmailError('Please enter a valid email address.');
+      valid = false;
+    }
+
+    if (!valid) {
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch('http://localhost:5000/api/submitForm', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name2, email2 }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setSubmitSuccess(true);
+        setSubmitError('');
+        toast.success('Form submitted successfully!');
+
+      } else {
+        setSubmitError(data.error || 'Error submitting the form.');
+        setSubmitSuccess(false);
+        toast.error(data.error || 'Error submitting the form.');
+
+      }
+    } catch (err) {
+      setSubmitError('Error submitting the form.');
+      setSubmitSuccess(false);
+      toast.error('Error submitting the form.');
+
+    }
+
+    setIsSubmitting(false);
+  };
 
   useEffect(() => {
-    const headerTextDelay = setTimeout(() => {
-      setShowHeaderText(true);
-    }, 1500);
-
-    const codeDoctorDelay = setTimeout(() => {
-      setShowCodeDoctor(true);
-    }, 2500);
-
-    const sentenceDelay = setTimeout(() => {
-      setShowSentence(true);
-    }, 3000);
-
-    const emailInputDelay = setTimeout(() => {
-      setShowEmailInputSection1(true);
-    }, 3500);
-
-    const imageDelay = setTimeout(() => {
-      setShowImage(true);
-    }, 4000);
-    const header2Delay = setTimeout(() => {
-      setShowHeader2(true);
-    }, 4500);
-    const header2SubtitleDelay = setTimeout(() => {
-      setShowHeader2Subtitle(true);
-    }, 5000);
-    const containerDelay = setTimeout(() => {
-      setShowContainer(true);
-    }, 5500);
-    const header3Delay = setTimeout(() => {
-      setShowHeader3(true);
-    }, 6000);
-
-    const cardGridDelay = setTimeout(() => {
-      setShowCardGrid(true);
-    }, 6500);
-
-    const header4Delay = setTimeout(() => {
-      setShowHeader4(true);
-    }, 7000);
-    const accessTextDelay = setTimeout(() => {
-      setShowAccessText(true);
-    }, 7500);
-    const emailInputDelay2 = setTimeout(() => {
-      setShowEmailInputSection2(true);
-    }, 8000);
-    const footerDelay = setTimeout(() => {
-      setShowFooter(true);
-    }, 8500);
-
-    const handleScroll = () => {
-      if (window.scrollY > 200) {
-        setScrollAnimation(true);
-      } else {
-        setScrollAnimation(false);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      clearTimeout(headerTextDelay);
-      clearTimeout(codeDoctorDelay);
-      clearTimeout(sentenceDelay);
-      clearTimeout(emailInputDelay);
-      clearTimeout(imageDelay);
-      clearTimeout(containerDelay);
-      clearTimeout(cardGridDelay);
-      clearTimeout(emailInputDelay2);
-      clearTimeout(header2Delay);
-      clearTimeout(header2SubtitleDelay);
-      clearTimeout(header3Delay);
-      clearTimeout(header4Delay);
-      clearTimeout(accessTextDelay);
-      clearTimeout(footerDelay);
-
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
+        const headerTextDelay = setTimeout(() => {
+          setShowHeaderText(true);
+        }, 1500);
+    
+        const codeDoctorDelay = setTimeout(() => {
+          setShowCodeDoctor(true);
+        }, 2500);
+    
+        const sentenceDelay = setTimeout(() => {
+          setShowSentence(true);
+        }, 3000);
+    
+        const emailInputDelay = setTimeout(() => {
+          setShowEmailInputSection1(true);
+        }, 3500);
+    
+        const imageDelay = setTimeout(() => {
+          setShowImage(true);
+        }, 4000);
+        const header2Delay = setTimeout(() => {
+          setShowHeader2(true);
+        }, 4500);
+        const header2SubtitleDelay = setTimeout(() => {
+          setShowHeader2Subtitle(true);
+        }, 5000);
+        const containerDelay = setTimeout(() => {
+          setShowContainer(true);
+        }, 5500);
+        const header3Delay = setTimeout(() => {
+          setShowHeader3(true);
+        }, 6000);
+    
+        const cardGridDelay = setTimeout(() => {
+          setShowCardGrid(true);
+        }, 6500);
+    
+        const header4Delay = setTimeout(() => {
+          setShowHeader4(true);
+        }, 7000);
+        const accessTextDelay = setTimeout(() => {
+          setShowAccessText(true);
+        }, 7500);
+        const emailInputDelay2 = setTimeout(() => {
+          setShowEmailInputSection2(true);
+        }, 8000);
+        const footerDelay = setTimeout(() => {
+          setShowFooter(true);
+        }, 8500);
+    
+        const handleScroll = () => {
+          if (window.scrollY > 200) {
+            setScrollAnimation(true);
+          } else {
+            setScrollAnimation(false);
+          }
+        };
+    
+        window.addEventListener('scroll', handleScroll);
+    
+        return () => {
+          clearTimeout(headerTextDelay);
+          clearTimeout(codeDoctorDelay);
+          clearTimeout(sentenceDelay);
+          clearTimeout(emailInputDelay);
+          clearTimeout(imageDelay);
+          clearTimeout(containerDelay);
+          clearTimeout(cardGridDelay);
+          clearTimeout(emailInputDelay2);
+          clearTimeout(header2Delay);
+          clearTimeout(header2SubtitleDelay);
+          clearTimeout(header3Delay);
+          clearTimeout(header4Delay);
+          clearTimeout(accessTextDelay);
+          clearTimeout(footerDelay);
+    
+          window.removeEventListener('scroll', handleScroll);
+        };
+      }, []);
 
   return (
-    <motion.div className="LandingPage">
+       <motion.div className="LandingPage">
       {/* Header */}
       <motion.div
         initial={{
@@ -196,7 +294,7 @@ export default function LandingPage() {
       </motion.div>
       <div className="line"></div>
 
-      {/* Main Content */}
+  
       <div className={`headerText ${scrollAnimation ? 'scroll-animation' : ''}`}>
         {showHeaderText && (
           <motion.p
@@ -267,16 +365,16 @@ export default function LandingPage() {
               whileFocus={{ scale: 1.2 }}
               className="emailInputText"
               placeholder="Your Name"
-              value={name}
-              onChange={handleNameChange}
+              value={name1}
+              onChange={handleNameChange1}
             />
             {nameError && <p className="errorText">{nameError}</p>}
             <motion.input
               whileFocus={{ scale: 1.2 }}
               className="emailInputText"
               placeholder="Your Email"
-              value={email}
-              onChange={handleEmailChange}
+              value={email1}
+              onChange={handleEmailChange1}
             />
             {emailError && <p className="errorText">{emailError}</p>}
           </div>
@@ -286,7 +384,7 @@ export default function LandingPage() {
               transition: { yoyo: Infinity },
             }}
             className="emailInputButton"
-            onClick={handleSubmit} // Handle form submission
+            onClick={handleSubmit1} // Handle form submission
           >
             Join Waitlist
           </motion.button>
@@ -578,39 +676,39 @@ export default function LandingPage() {
         </motion.div>
       )}
 
-{showEmailInputSection2 && (
-        <motion.div className="emailInput">
-          <div>
-            <motion.input
-              whileFocus={{ scale: 1.2 }}
-              className="emailInputText"
-              placeholder="Your Name"
-              value={name}
-              onChange={handleNameChange}
-            />
-            {nameError && <p className="errorText">{nameError}</p>}
-            <motion.input
-              whileFocus={{ scale: 1.2 }}
-              className="emailInputText"
-              placeholder="Your Email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-            {emailError && <p className="errorText">{emailError}</p>}
-          </div>
-          <motion.button
-            whileHover={{
-              scale: 1.2,
-              transition: { yoyo: Infinity },
-            }}
-            className="emailInputButton"
-            onClick={handleSubmit} // Handle form submission
-          >
-            Join Waitlist
-          </motion.button>
-        </motion.div>
-      )}
-{showFooter && (
+    {showEmailInputSection2 && (
+            <motion.div className="emailInput">
+              <div>
+                <motion.input
+                  whileFocus={{ scale: 1.2 }}
+                  className="emailInputText"
+                  placeholder="Your Name"
+                  value={name2}
+                  onChange={handleNameChange2}
+                />
+                {nameError && <p className="errorText">{nameError}</p>}
+                <motion.input
+                  whileFocus={{ scale: 1.2 }}
+                  className="emailInputText"
+                  placeholder="Your Email"
+                  value={email2}
+                  onChange={handleEmailChange2}
+                />
+                {emailError && <p className="errorText">{emailError}</p>}
+              </div>
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                  transition: { yoyo: Infinity },
+                }}
+                className="emailInputButton"
+                onClick={handleSubmit2} // Handle form submission
+              >
+                Join Waitlist
+              </motion.button>
+            </motion.div>
+          )}
+    {showFooter && (
         <motion.div
           initial={{
             opacity: 0,
@@ -630,6 +728,9 @@ export default function LandingPage() {
           Copyright © <span>2023 EXOCODE TECHNOLOGIES</span> | All rights reserved
         </motion.div>
       )}
+      <ToastContainer position="top-center"/>
     </motion.div>
+    
+
   );
 }
